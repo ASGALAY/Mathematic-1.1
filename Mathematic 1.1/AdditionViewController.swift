86984ab879d5531a341396a3ccf649691b836275
ASGALAY <asgala@mail.ru>
2015-01-13 15:49:09 +0300

    Начало работы с Git
    Начало работы с Git

---
+++ Mathematic 1.1/AdditionViewController.swift
@@ -0,0 +1,24 @@
+//
+//  AdditionViewController.swift
+//  Mathematic 1.1
+//
+//  Created by Анатолий Галай on 06.12.14.
+//  Copyright (c) 2014 Father & Son. All rights reserved.
+//
+
+import UIKit
+
+class AdditionViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
---
+++ Mathematic 1.1/MainMenuViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SpecialViewController.swift
+//  Mathematic 1.1
+//
+//  Created by Анатолий Галай on 03.12.14.
+//  Copyright (c) 2014 Father & Son. All rights reserved.
+//
+
+import UIKit
+
+class MainMenuViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
